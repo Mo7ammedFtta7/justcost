3d09589497c048a35b6da29267d38cb21872682a
Mo7ammedFtta7 <M fttah>
2019-05-24 23:02:10 +0400

    new add

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
+import { EventsComponent } from './events/events.component';
+import { HomeComponent} from './home/home.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'special',
+    canActivate: [AuthGuard],
+    component: SpecialEventsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
