a84993fb82a0140103493964d579c2f49298563e
yaseenomer <yaseeno373@gmail.com>
2019-11-20 03:06:39 -0800

    all

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -42,7 +42,7 @@ export class ResultsComponent implements OnInit {
       });
       this.getAttributes(this.paramsa);
     });
-   //  console.log(this.auth.user().userInfo.likedProducts);
+    console.log(this.auth.user());
   }
 
   public math(aa: any, bb: any) {
