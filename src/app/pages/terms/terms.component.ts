91755f088ea82cba4f2f7a407769fca64813e58d
sahlowle <mis.gxr@gmail.com>
2019-12-21 17:34:53 +0200

    gg

--- src/app/pages/terms/terms.component.ts
+++ src/app/pages/terms/terms.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ApiService} from '../../_services/api.service';
+import { TranslateService } from '../../pipe/translate.service';
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.css']
 })
 export class TermsComponent implements OnInit {
-
-  constructor() { }
+  data:any;
+  constructor(private api:ApiService,public translte:TranslateService) { }
 
   ngOnInit() {
+    // **  (API) get Terms
+    this.api.get('terms').subscribe(
+      next => {
+        this.data = next.data;
+      }
+    );
+    // ** end Of Api
   }
 
 }
