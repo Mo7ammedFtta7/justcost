d8ddba14318727e952a6e7ab394d5f4125e186c3
sahlowle <mis.gxr@gmail.com>
2019-12-28 10:52:05 +0200

    d

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,28 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ApiService } from '../../_services/api.service';
 import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '../../pipe/translate.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor(private api: ApiService,private toastr:ToastrService) { }
+  submitting = false;
+  data:any;
+  constructor(private api: ApiService,private toastr:ToastrService,public translate:TranslateService) { }
 
   ngOnInit() {
+    this.api.get('about_pages').subscribe(
+      next => {
+        this.data = next.data;
+      }
+    );
   }
-  submitForm(form:NgForm){
+  submitForm(form:NgForm) { // ** submity form
     if(form.invalid){
       return;
     }
+    this.submitting = true;
     let payload = form.value;
-    this.api.post('',payload).subscribe(
+    this.api.post('contact-us',payload).subscribe(
       next => {
+        this.submitting = false;
         this.toastr.success('thank you for contact us');
         form.resetForm();
       }
     );
-  }
+  } // ** End OF Submit From
 
 }
